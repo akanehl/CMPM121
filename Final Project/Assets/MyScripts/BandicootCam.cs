52c88af5ff697821960ea5d6d9ca0bea89573b66
akanehl <akanehl@ucsc.edu>
2019-03-04 21:22:45 -0800

    Initial alpha release
    Added BandicootCam, Added Test environment, added collectible bolts/nuts, added animations and movement.

---
+++ Final Project/Assets/MyScripts/BandicootCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandicootCam : MonoBehaviour
+{
+    public GameObject player;
+    private float offset;
+    private Vector3 zOnly;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position.z;
+        Camera.main.transform.rotation = Quaternion.Euler(30, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zOnly = new Vector3(0, 3 ,player.transform.position.z + offset);
+        transform.position = zOnly;
+        
+    }
+}
